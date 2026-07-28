07b6552bd2577017ff2d294ff70553ed4b2dfde9
Thomas Ho <khoaho120703@gmail.com>
2026-07-28 15:20:38 +0700

    [client & server] create db & config nextjs project

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,49 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      // Cloudinary
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**",
+      },
+
+      // GitHub Avatar (nếu dùng OAuth GitHub)
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        pathname: "/**",
+      },
+
+      // Google Avatar
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.dummyjson.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.dummyjson.com",
+        pathname: "/**",
+      },
+    ],
+
+    formats: ["image/avif", "image/webp"],
+
+    minimumCacheTTL: 60 * 60 * 24 * 30,
+  },
 };
 
 export default nextConfig;
---
+++ prisma.config.ts
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { defineConfig } from "prisma/config";
+
+const prismaUrl = process.env.DIRECT_URL || process.env.DATABASE_URL;
+if (!prismaUrl) {
+  throw new Error(
+    "DIRECT_URL or DATABASE_URL must be set in .env for Prisma CLI commands.",
+  );
+}
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "tsx prisma/seed.ts",
+  },
+  datasource: {
+    url: prismaUrl,
+  },
+});
